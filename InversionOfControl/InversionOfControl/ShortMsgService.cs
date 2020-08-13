5d7cd234c14c6593594e880cb1c24876972e1d83
rzhalivtsiv <rostyslav.zhalivtsiv@gmail.com>
2020-08-13 11:35:53 +0300

    Add new implementations of abstraction

---
+++ InversionOfControl/InversionOfControl/ShortMsgService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InversionOfControl
+{
+	class ShortMsgService : IMessagingService
+	{
+		public void SendMessage(string text)
+		{
+			Console.WriteLine("Message send by SMS");
+			Console.WriteLine($"Text: {text} \n");
+		}
+	}
+}
---
+++ InversionOfControl/InversionOfControl/TelegramMessegerAPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InversionOfControl
+{
+	class TelegramMessegerAPI : IMessagingService
+	{
+		public void SendMessage(string text)
+		{
+			Console.WriteLine("Message send by Telegram API v.14.6");
+			Console.WriteLine($"Text: {text} \n");
+		}
+	}
+}
